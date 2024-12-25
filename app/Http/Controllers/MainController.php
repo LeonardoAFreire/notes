779661647f723ac66a124377b3ce14c4b4cd0e0a
leo <leoaguiarfreire@gmail.com>
2024-12-25 13:24:25 -0300

    adicao de notas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
+
 
 class MainController extends Controller
 {
@@ -11,19 +14,59 @@ class MainController extends Controller
   {
     // load user's notes
     $id = session('user.id');
-    $user = User::find($id)->toArray();
     $notes = User::find($id)->notes()->get()->toArray();
 
-
-    echo '<pre>';
-    print_r($user);
-    print_r($notes);
-
-    return view('home');
+    return view('home', compact('notes'));
   }
 
   public function newNote()
   {
-    echo 'Im creating a new note';
+    // show newNote view
+    return view('new_note');
+  }
+
+  public function newNoteSubmit(Request $request)
+  {
+
+    // validate request
+    $request->validate(
+      [
+        'text_title' => 'required|min:3|max:200',
+        'text_note' => 'required|min:3|max:3000'
+      ],
+      // Messages errors
+      [
+        'text_title.required' => 'Title is required.',
+        'text_title.min' => 'Title must have more than :min characters.',
+        'text_title.max' => 'Title must have no more than :max characters.',
+        'text_note.required' => 'Note is required.',
+        'text_note.min' => 'Note must have more than :min characters.',
+        'text_note.max' => 'Note must have no more than :max characters.',
+      ]
+    );
+
+    // get user id
+    $id = session('user.id');
+    // create new note
+    $note = new Note();
+    $note->user_id = $id;
+    $note->title = $request->text_title;
+    $note->text = $request->text_note;
+    $note->save();
+
+    // redirect to home
+    return redirect()->route('home');
+  }
+
+
+  public function editNote($id)
+  {
+    $id = Operations::decryptId($id);
+  }
+
+
+  public function deleteNote($id)
+  {
+    $id = Operations::decryptId($id);
   }
 }
