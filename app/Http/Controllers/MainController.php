fd080c69250d9b51d5803ae31c70e707b409da64
leo <leoaguiarfreire@gmail.com>
2024-12-25 03:48:46 -0300

    middleware checkislogged

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,4 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController extends Controller {}
+class MainController extends Controller
+{
+  public function index()
+  {
+    echo 'Im inside the app';
+  }
+
+  public function newNote()
+  {
+    echo 'Im creating a new note';
+  }
+}
